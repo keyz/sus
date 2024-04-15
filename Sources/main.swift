bd98d5c5ed1fdf1500e6a459de2e238f86cb9750
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2024-04-14 23:22:15 -0700

    listen to hot key

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,6 +1,27 @@
+import Cocoa
+import Foundation
+import KeyboardShortcuts
+
 @main
 struct Main {
     static func main() {
-        print("sus")
+        let app = NSApplication.shared
+
+        KeyboardShortcuts.setShortcut(
+            .init(.r, modifiers: [.command, .option]),
+            for: .restart
+        )
+
+        KeyboardShortcuts.onKeyUp(for: .restart, action: {
+            print("hot key pressed: restart")
+            app.stop(nil)
+        })
+
+        print("listening...")
+        app.run()
     }
+}
+
+extension KeyboardShortcuts.Name {
+    static let restart = Self("restart")
 }
