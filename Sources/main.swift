163e95da3468f8e15bdbc34ad5d649b1c6503b0f
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2024-04-14 23:58:58 -0700

    basic working version

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,3 +1,4 @@
+import ArgumentParser
 import Cocoa
 import Foundation
 import KeyboardShortcuts
@@ -5,20 +6,67 @@ import KeyboardShortcuts
 @main
 struct Main {
     static func main() {
+        Sus.main()
+    }
+}
+
+struct Sus: ParsableCommand {
+    @Argument(help: "TODO")
+    var command: [String]
+
+    static var configuration = CommandConfiguration(
+        abstract: "TODO"
+    )
+
+    func run() throws {
         let app = NSApplication.shared
 
+        setupHotKeys()
+
+        // Continue running the process in an infinite management cycle
+        NotificationCenter.default.addObserver(forName: Process.didTerminateNotification, object: nil, queue: nil) { _ in
+            print("Process terminated. Ready to restart.")
+        }
+
+        // Start the initial process
+        startOrRestartProcess(command: command)
+
+        app.run()
+    }
+
+    private func setupHotKeys() {
         KeyboardShortcuts.setShortcut(
             .init(.r, modifiers: [.command, .option]),
             for: .restart
         )
 
-        KeyboardShortcuts.onKeyUp(for: .restart, action: {
-            print("hot key pressed: restart")
-            app.stop(nil)
-        })
+        print("Listening on hotkey: \(KeyboardShortcuts.getShortcut(for: .restart)!)")
 
-        print("listening...")
-        app.run()
+        KeyboardShortcuts.onKeyUp(for: .restart) {
+            print("Hotkey pressed: Restarting process...")
+            startOrRestartProcess(command: command)
+        }
+    }
+
+    private func startOrRestartProcess(command: [String]) {
+        guard !command.isEmpty else {
+            print("Error: No command provided to execute.")
+            return
+        }
+
+        let process = Process()
+        process.launchPath = "/usr/bin/env" // Use `env` to find the command in the PATH
+        process.arguments = command
+
+        process.standardOutput = FileHandle.standardOutput
+        process.standardError = FileHandle.standardError
+        process.standardInput = FileHandle.standardInput
+
+        process.terminationHandler = { _ in
+            print("Process has terminated. Use hotkey to restart.")
+        }
+
+        process.launch()
     }
 }
 
